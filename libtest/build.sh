bb509ea55f68ba3d7a4b346efae1e105f62d652d
Zenny Chen <zenny_chen@sina.com>
2023-03-01 23:35:29 +0800

    Update build.sh

--- libtest/build.sh
+++ libtest/build.sh
@@ -5,11 +5,12 @@ common_cxx_options="-std=gnu++17"
 gcc static_2.S -o static_2.S.o -fPIC -c 
 gcc static_1.c -o static_1.c.o -fPIC ${common_c_options} -c
 gcc alib.c -o alib.c.o ${common_c_options} -c
+gcc blib.c -o blib.c.o ${common_c_options} -c
 # Generate the static library
 ar crs libtest-static.a static_2.S.o static_1.c.o
 ar -crv libalib.a alib.c.o
 ar -crv libblib.a blib.c.o
-rm static_1.c.o static_2.S.o
+rm static_1.c.o static_2.S.o alib.c.o blib.c.o
 # Compile sources for the shared library
 gcc dynamic_1.c -o dynamic_1.c.o -fPIC ${common_c_options} -c
 g++ dynamic_2.cpp -o dynamic_2.cpp.o -fPIC ${common_cxx_options} -c
@@ -19,4 +20,5 @@ rm dynamic_1.c.o dynamic_2.cpp.o
 # Compile and generate the executable
 # ATTENTION! The sequence of `libalib.a` and `libblib.a` MUST NOT be reverse.
 # Here, "libalib.a" depends on "libblib.a"
+# You may also use: `gcc main.c -o test ${common_c_options} -L./ -lalib -lblib -ltest-shared -lstdc++`
 gcc main.c libalib.a libblib.a -o test ${common_c_options} -L./ -ltest-shared -lstdc++
