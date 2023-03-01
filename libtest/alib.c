c7fa4ae7ef6d24d7b9efd7557df86a937fae02cd
Zenny Chen <zenny_chen@sina.com>
2023-03-01 22:59:22 +0800

    Add files via upload

---
+++ libtest/alib.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern void BFunc(int a);
+
+void AFunc(int a)
+{
+    BFunc(a * 2);
+}
---
+++ libtest/blib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void BFunc(int a)
+{
+    printf("input is: %d\n", a);
+}
