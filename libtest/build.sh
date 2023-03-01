56c346da890cd730fab00d08a4576fc616621937
Zenny Chen <zenny_chen@sina.com>
2023-03-01 23:27:09 +0800

    Update build.sh

--- libtest/build.sh
+++ libtest/build.sh
@@ -4,8 +4,11 @@ common_cxx_options="-std=gnu++17"
 # Compile sources for the static library
 gcc static_2.S -o static_2.S.o -fPIC -c 
 gcc static_1.c -o static_1.c.o -fPIC ${common_c_options} -c
+gcc alib.c -o alib.c.o ${common_c_options} -c
 # Generate the static library
 ar crs libtest-static.a static_2.S.o static_1.c.o
+ar -crv libalib.a alib.c.o
+ar -crv libblib.a blib.c.o
 rm static_1.c.o static_2.S.o
 # Compile sources for the shared library
 gcc dynamic_1.c -o dynamic_1.c.o -fPIC ${common_c_options} -c
@@ -14,4 +17,6 @@ g++ dynamic_2.cpp -o dynamic_2.cpp.o -fPIC ${common_cxx_options} -c
 gcc dynamic_1.c.o dynamic_2.cpp.o -o libtest-shared.so -shared -L./ -lstdc++ -ltest-static
 rm dynamic_1.c.o dynamic_2.cpp.o
 # Compile and generate the executable
-gcc main.c -o test ${common_c_options} -L./ -ltest-shared -lstdc++
+# ATTENTION! The sequence of `libalib.a` and `libblib.a` MUST NOT be reverse.
+# Here, "libalib.a" depends on "libblib.a"
+gcc main.c libalib.a libblib.a -o test ${common_c_options} -L./ -ltest-shared -lstdc++
